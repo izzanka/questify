32b15f7befc2b7ae881d3837d065e1e4169c0a1b
Lana <chewygum56@gmail.com>
2023-05-02 15:28:37 +0700

    update signin validation

--- asset/js/signin.js
+++ asset/js/signin.js
@@ -1,17 +1,39 @@
 let myAlert = document.getElementById('myAlert')
 let alertMessage = document.getElementById('alertMessage')
+let txtEmail = document.getElementById('txtEmail')
+let txtPassword = document.getElementById('txtPassword')
+let btnSignIn = document.getElementById('btnSignIn')
+let btnAlertClose = document.getElementById('btnAlertClose')
 
-function validate() {
-    let email = document.getElementById('txtEmail')
-    let password = document.getElementById('txtPassword')
-
-    if(email.value != 'admin') {
-        myAlert.classList.remove('collapse')
-        alertMessage.innerHTML = 'Email or password do not match'
+btnSignIn.addEventListener('click', function() {
+    if(txtEmail.value == null || txtEmail.value.length == 0) {
+        showAlert('Email must be filled')
+        return
     }
 
-    if(password.value != 'admin123') {
-        myAlert.classList.remove('collapse')
-        alertMessage.innerHTML = 'Email or password do not match'
+    if(txtPassword.value == null || txtPassword.value.length == 0) {
+        showAlert('Password must be filled')
+        return
     }
+
+    if(txtEmail.value != 'admin@gmail.com') {
+        showAlert('Email or password do not match')
+        return
+    }
+
+    if(txtPassword.value != 'admin123') {
+        showAlert('Email or password do not match')
+        return
+    }
+    
+    window.location.href = 'home.html'
+})
+
+btnAlertClose.addEventListener('click', function() {
+    myAlert.classList.add('collapse')
+})
+
+function showAlert(message) {
+    myAlert.classList.remove('collapse')
+    alertMessage.innerHTML = message
 }
